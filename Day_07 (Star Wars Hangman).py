77aeb9cc9c5d94f5e221ff41de5088b5987a368a
Samuel Ordunez <84251206+s-ordunez@users.noreply.github.com>
2025-06-13 04:16:20 -0700

    Update Day_07 (Star Wars Hangman).py
    cleaned up the ascii art and lists and moved them to separate file.
    also added more ascii art

--- Day_07 (Star Wars Hangman).py
+++ Day_07 (Star Wars Hangman).py
@@ -1,81 +1,10 @@
 import random
-
-the_hanged_man = ['''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\\  |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\\  |
- /    |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\\  |
- / \\  |
-      |
-=========
-YOU LOSE!!!''']
-
-word_list = ['star wars', 'darth maul', 'darth vader', 'the force',
-             'luke', 'leia', 'han solo','chewbacca','palpatine',
-             'death star', 'yoda', 'tatooine', 'alderaan', 'naboo',
-             'jedi', 'sith', 'empire', 'rebellion', 'lightsaber',
-             'millenium falcon', 'jar jar binks', 'padawan', 
-             'droid', 'pod racing', 'i am your father',
-             'stormtrooper', 'jabba the hutt', 'the dark side',
-             'ashoka', 'andor', 'i have a bad feeling about this',
-             'never tell me the odds', 'i am your father', 
-             'may the force be with you', 'a new hope', 
-             'the empire strikes back', 'return of the jedi',
-             'the phantom menace', 'attack of the clones',
-             'the clone wars', 'revenge of the sith', 
-             'the force awakens', 'the last jedi', 
-             'the rise of skywalker', 'thats no moon', 'ewok',
-             'wookie', 'scruffy looking nerf herder', 'hoth,'
-             'coruscant', 'mustafar', 'kamino', 'geonosis',
-             'dagobah', 'jakku', 'mon calamari', 'admiral ackbar',
-             'its a trap', 'anakin', 'padme', 'the senate', 'grogu',
-             'mandalore', 'boba fett', 'django fett', 'mandalorian',
-             'beskar', 'darth bane', 'mando', 'darth sidious', 
-             'younglings', 'moff gideon', 'grand moff tarkin']
+from Day_07_ascii_art import (starwars, hangman, the_hanged_man, word_list,
+                              vader, player_choices)
 
 play_again = "y"
+print(starwars)
+print(hangman)
 while play_again == "y":
 
     chosen_word = random.choice(word_list)
@@ -97,10 +26,6 @@ while play_again == "y":
             new_chosen_word_progress.append("_")
 
     print("Word to guess: " + ''.join(chosen_word_progress))
-
-    player_choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-                      'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-                      'u', 'v', 'w', 'x', 'y', 'z']
 
     tries = 0
     while tries < 7:
@@ -128,6 +53,7 @@ while play_again == "y":
                 tries += 1
                 print("Hey that's cheating!")
                 if tries == 7:
+                    print(vader)
                     print("The word was " + ''.join(chosen_word).upper())
             else:
                 print("You already chose that.")
@@ -137,6 +63,7 @@ while play_again == "y":
             tries += 1
             print(''.join(chosen_word_progress).upper())
             if tries == 7:
+                print(vader)
                 print("The word was "+''.join(chosen_word).upper())
 
         print("****************************"+str(7-tries)+"/7 LIVES LEFT****************************")
