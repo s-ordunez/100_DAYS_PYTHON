865d74fa4a557bf7583e886bc637946d2fd6a8f0
Samuel Ordunez <84251206+s-ordunez@users.noreply.github.com>
2025-06-13 04:17:22 -0700

    Add files via upload
    contains the ascii art and word lists for the day 7 hangman game

---
+++ Day_07_ascii_art.py
@@ -0,0 +1,136 @@
+
+starwars = '''
+             .               .    .          .              .   .         .
+               _________________      ____         __________
+ .       .    /                 |    /    \\    .  |          \\
+     .       /    ______   _____| . /      \\      |    ___    |     .     .
+             \\    \\    |   |       /   /\\   \\     |   |___>   |
+           .  \\    \\   |   |      /   /__\\   \\  . |         _/               .
+ .     ________>    |  |   | .   /            \\   |   |\\    \\_______    .
+      |            /   |   |    /    ______    \\  |   | \\           |
+      |___________/    |___|   /____/      \\____\\ |___|  \\__________|    .
+  .     ____    __  . _____   ____      .  __________   .  _________
+       \\    \\  /  \\  /    /  /    \\       |          \\    /         |      .
+        \\    \\/    \\/    /  /      \\      |    ___    |  /    ______|  .
+         \\              /  /   /\\   \\ .   |   |___>   |  \\    \\
+   .      \\            /  /   /__\\   \\    |         _/.   \\    \\            +
+           \\    /\\    /  /            \\   |   |\\    \\______>    |   .
+            \\  /  \\  /  /    ______    \\  |   | \\              /          .
+ .       .   \\/    \\/  /____/      \\____\\ |___|  \\____________/    
+                               .                                        .
+     .                           .         .               .                 .
+                .                                   .            .
+'''
+hangman = '''
+                 _                                             
+                | |                                            
+                | |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+                | '_ \\ / _` | '_ \\ / _` | '_ ` _ \\ / _` | '_ \\ 
+                | | | | (_| | | | | (_| | | | | | | (_| | | | |
+                |_| |_|\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_|
+                                    __/ |                      
+                                   |___/                       
+'''
+
+the_hanged_man = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\\  |
+ / \\  |
+      |
+=========
+YOU LOSE!!!
+''']
+
+vader = '''
+                       .-.
+                      |_:_|
+                     /(_Y_)\\
+.                   ( \\/M\\/ )
+ '.               _.'-/'-'\\-'._
+   ':           _/.--'[[[[]'--.\\_
+     ':        /_'  : |::"| :  '.\\
+       ':     //   ./ |oUU| \\.'  :\\
+         ':  _:'..' \\_|___|_/ :   :|
+           ':.  .'  |_[___]_|  :.':\\
+            [::\\ |  :  | |  :   ; : \\
+             '-'   \\/'.| |.' \\  .;.' |
+             |\\_    \\  '-'   :       |
+             |  \\    \\ .:    :   |   |
+             |   \\    | '.   :    \\  |
+             /       \\   :. .;       |
+            /     |   |  :__/     :  \\
+           |  |   |    \\:   | \\   |   ||
+          /    \\  : :  |:   /  |__|   /|
+          |     : : :_/_|  /'._\\  '--|_\\
+          /___.-/_|-'   \\  \\ '-'          '''
+
+word_list = ['star wars', 'darth maul', 'darth vader', 'the force',
+             'luke', 'leia', 'han solo','chewbacca','palpatine',
+             'death star', 'yoda', 'tatooine', 'alderaan', 'naboo',
+             'jedi', 'sith', 'empire', 'rebellion', 'lightsaber',
+             'millenium falcon', 'jar jar binks', 'padawan',
+             'droid', 'pod racing', 'i am your father',
+             'stormtrooper', 'jabba the hutt', 'the dark side',
+             'ashoka', 'andor', 'i have a bad feeling about this',
+             'never tell me the odds', 'i am your father',
+             'may the force be with you', 'a new hope',
+             'the empire strikes back', 'return of the jedi',
+             'the phantom menace', 'attack of the clones',
+             'the clone wars', 'revenge of the sith',
+             'the force awakens', 'the last jedi',
+             'the rise of skywalker', 'thats no moon', 'ewok',
+             'wookie', 'scruffy looking nerf herder', 'hoth,'
+             'coruscant', 'mustafar', 'kamino', 'geonosis',
+             'dagobah', 'jakku', 'mon calamari', 'admiral ackbar',
+             'its a trap', 'anakin', 'padme', 'the senate', 'grogu',
+             'mandalore', 'boba fett', 'django fett', 'mandalorian',
+             'beskar', 'darth bane', 'mando', 'darth sidious',
+             'younglings', 'moff gideon', 'grand moff tarkin']
+
+player_choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                      'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                      'u', 'v', 'w', 'x', 'y', 'z']
