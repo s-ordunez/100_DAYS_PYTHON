44254c41e1a4014db1e5ce1cc366b72c0ad97a8c
Samuel Ordunez <84251206+s-ordunez@users.noreply.github.com>
2025-06-12 20:53:22 -0700

    day 6 complete, using while loops

---
+++ Day_06 (Escaping the Maze).py
@@ -0,0 +1,16 @@
+### This challenge uses reeborgs world to find an exit for the robot
+### The website has some predefined functions and maps to use
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
